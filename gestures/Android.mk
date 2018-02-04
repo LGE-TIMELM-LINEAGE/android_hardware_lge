9b34b95c9d4bfabf4d30985ede3539b535f16e12
Rashed Abdel-Tawab <rashed@linux.com>
2018-02-04 14:29:32 -0800

    lg/gesture: Explicitly include support libraries
    Change-Id: I17476e279a04a409a873f62c206fb3edf77ba546

--- gestures/Android.mk
+++ gestures/Android.mk
@@ -5,6 +5,10 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v13 \
+    android-support-v7-appcompat \
+    android-support-v7-preference \
+    android-support-v7-recyclerview \
+    android-support-v14-preference \
     org.lineageos.platform.internal
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
