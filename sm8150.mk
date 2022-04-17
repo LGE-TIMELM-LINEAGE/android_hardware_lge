f29eee21ac38a404bbb445e2aa6e387056807038
Anh Tran <anh.tran.3889@gmail.com>
2022-04-18 02:06:06 +0700

    sm8150-common: Build Tetheroffload

--- sm8150.mk
+++ sm8150.mk
@@ -500,6 +500,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Tetheroffload
+PRODUCT_PACKAGES += \
+    android.hardware.tetheroffload.config@1.0.vendor \
+    android.hardware.tetheroffload.control@1.0.vendor
+
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
