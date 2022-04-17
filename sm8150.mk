418faab262404cb7fdcbc699c5f4358977331879
Anh Tran <anh.tran.3889@gmail.com>
2022-04-18 02:02:32 +0700

    sm8150-common: Build android.hardware.power@1.2.vendor

--- sm8150.mk
+++ sm8150.mk
@@ -417,6 +417,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power@1.2.vendor \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.2.vendor
 
