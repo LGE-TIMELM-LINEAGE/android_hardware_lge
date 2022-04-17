d1e2c60c04081446fde1026b37d6430566e14783
Anh Tran <anh.tran.3889@gmail.com>
2022-04-18 00:50:23 +0700

    sm8150-common: Spilt manifests to manifests-qva

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -120,6 +120,7 @@ USE_DEVICE_SPECIFIC_GPS := true
 # HIDL
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest-qva.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_lge_msmnile
