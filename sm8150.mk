d7cbcbb6275775f1f232f6f4971d7583407f328c
Anh Tran <anh.tran.3889@gmail.com>
2022-04-18 01:55:05 +0700

    sm8150-common: Update Camera packages for A12

--- sm8150.mk
+++ sm8150.mk
@@ -163,10 +163,11 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    libxml2 \
+    libcamera2ndk_vendor \
+    libdng_sdk.vendor \
+    libgui_vendor \
     Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
