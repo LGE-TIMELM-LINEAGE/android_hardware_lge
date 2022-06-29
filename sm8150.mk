7ad6e56a05dd05521a49a31b8fe2562f00d0fabb
Andrzej Perczak <linux@andrzejperczak.com>
2022-06-29 10:33:07 +0000

    sm8150-common: Import lmkd props from google gki
    Reference: https://github.com/LineageOS/android_hardware_google_pixel/blob/lineage-19.1/mm/device_gki.mk

--- sm8150.mk
+++ sm8150.mk
@@ -300,6 +300,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.lge_msmnile
 
+# LMK tuning
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lmk.filecache_min_kb=153600 \
+    ro.lmk.stall_limit_critical=40
+
 # Media
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
