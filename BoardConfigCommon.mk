6ab1928ec4e835df9a8eac015da37d781ebea07c
Anh Tran <anh.tran.3889@gmail.com>
2022-08-18 23:32:53 +0700

    sm8150-common: Stop enabling memcg

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -138,7 +138,6 @@ TARGET_USES_ION := true
 BOARD_USES_METADATA_PARTITION := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE += androidboot.memcg=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048
