fc2ad5aba7048718b204fa5a262d3e7157055e24
Han Sol Jin <hansol@hansol.ca>
2023-02-20 20:09:56 -0800

    hardware/lge: sepolicy: add a SEPolicy.mk
    Change-Id: Ie1c8ceeb57dba6de1efc353962b8a0e97927a555

---
+++ sepolicy/SEPolicy.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2022-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += hardware/lge/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += hardware/lge/sepolicy/public
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/lge/sepolicy/vendor
